d8acffa38ae3e8a2bcb53cf95adbcabfe1f343e5
KamyaPA <peteraksel@live.dk>
2023-10-31 11:03:24 +0100

    Axum hello world, initial setup

---
+++ ecdar-web/src/main.rs
@@ -0,0 +1,11 @@
+use axum::*;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(|| async { "Hello World" }));
+
+    Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
