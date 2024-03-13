74851fa5b23010c99e1a7ef308438084733433e0
Thomas Lohse <49527735+t-lohse@users.noreply.github.com>
2024-03-13 17:28:49 +0100

    SW5 (#2)
    * major refactor
    
    * major refactor
    
    * added submodule
    
    * fix naming in CI
    
    * Update Cargo.toml
    
    * Update Cargo.toml
    
    * Update Cargo.toml
    
    * Update Cargo.toml
    
    * Update build_artifacts.yml
    
    * Update build_artifacts.yml
    
    * Update build_artifacts.yml
    
    * Update build_artifacts.yml
    
    * Update build_artifacts.yml
    
    * Update build_artifacts.yml
    
    * Update build_artifacts.yml
    
    * Update build_artifacts.yml
    
    * Update build_artifacts.yml
    
    * Update check_format.yml
    
    * Update test.yml
    
    * Update main.rs
    
    * Update build_artifacts.yml
    
    * Update .gitignore
    
    * Delete .idea directory
    
    * Update .gitmodules
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update .gitmodules

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
+use convert_case::*;
 use ecdar_protobuf_transpiler::CompileVariables;
 use proc_macro::TokenStream as TS;
 use proc_macro2::TokenStream;
 use quote::quote;
-use convert_case::*;
 
 #[proc_macro]
-pub fn add_endpoints(app_name: TS) -> TS{
+pub fn add_endpoints(app_name: TS) -> TS {
     let app_name = TokenStream::from(app_name);
 
     let endpoints = ecdar_protobuf_transpiler::compile(|var| {
@@ -16,24 +16,26 @@ pub fn add_endpoints(app_name: TS) -> TS{
             ..
         } = var;
 
-        let route = format!("/{}/{}", 
-            service_name.to_string().to_case(Case::Snake), 
+        let route = format!(
+            "/{}/{}",
+            service_name.to_string().to_case(Case::Snake),
             endpoint_name.to_string().to_case(Case::Snake)
         );
 
-        quote!{
+        quote! {
             route(#route, post(#fn_name))
         }
     });
 
-    quote!{
+    quote! {
         #app_name.#(#endpoints).*
-    }.into()
+    }
+    .into()
 }
 
 #[proc_macro]
-pub fn add_endpoint_functions(_: TS) -> TS{
-    let functions = ecdar_protobuf_transpiler::compile(|var|{
+pub fn add_endpoint_functions(_: TS) -> TS {
+    let functions = ecdar_protobuf_transpiler::compile(|var| {
         let CompileVariables {
             in_struct,
             in_struct_name,
@@ -46,12 +48,12 @@ pub fn add_endpoint_functions(_: TS) -> TS{
         } = var;
 
         let payload_body = if in_struct_has_body {
-            quote!{ payload.body }
-        }  else {
-            quote!{()}
+            quote! { payload.body }
+        } else {
+            quote! {()}
         };
-        
-        quote!{
+
+        quote! {
             #in_struct
             pub async fn #fn_name(Json(payload) : Json<#in_struct_name>)
                 -> Result<Json<#rtn_struct>, StatusCode> {
@@ -67,8 +69,8 @@ pub fn add_endpoint_functions(_: TS) -> TS{
         }
     });
 
-    quote!{
+    quote! {
         #(#functions)*
-    }.into()
+    }
+    .into()
 }
-
